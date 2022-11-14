f0012ae8cb399053005dbd2a28d3255b2fa72f78
Derrick Hammer <derrick@derrickhammer.com>
2022-11-14 11:09:02 -0500

    *Initial version

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+import ProtomuxRPC from "protomux-rpc"
+// @ts-ignore
+import c from "compact-encoding"
+import b4a from "b4a"
+
+const ID = b4a.from("lumeweb")
+
+export default class RPC extends ProtomuxRPC {
+    constructor(stream: any, options = {}) {
+        options = {
+            ...{
+                id: ID,
+                valueEncoding: c.json
+            }, ...options
+        };
+        super(stream, options);
+    }
+}
