201d4fe91cb5359d58ec49b6f30ee0ff5551fb14
Derrick Hammer <derrick@derrickhammer.com>
2022-11-14 11:09:45 -0500

    *add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import ProtomuxRPC from "protomux-rpc";
+export default class RPC extends ProtomuxRPC {
+    constructor(stream: any, options?: {});
+}
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+// @ts-ignore
+import ProtomuxRPC from "protomux-rpc";
+// @ts-ignore
+import c from "compact-encoding";
+import b4a from "b4a";
+const ID = b4a.from("lumeweb");
+export default class RPC extends ProtomuxRPC {
+    constructor(stream, options = {}) {
+        options = {
+            ...{
+                id: ID,
+                valueEncoding: c.json
+            }, ...options
+        };
+        super(stream, options);
+    }
+}
