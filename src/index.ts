49fc39ab08224681c92f6357fe855a17cb8b8374
Derrick Hammer <derrick@derrickhammer.com>
2022-11-27 23:59:33 -0500

    *protomux will automatically encode data to JSON, so just set value default to empty string

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,7 @@ export default class RPC extends ProtomuxRPC {
     super(stream, options);
   }
 
-  async request(
-    method: any,
-    value: Buffer | Uint8Array | string = b4a.from(""),
-    options = {}
-  ) {
-    if (!b4a.isBuffer(value)) {
-      value = b4a.from(value);
-    }
-
+  async request(method: any, value: any | string = "", options = {}) {
     return super.request(method, value, options);
   }
 }
